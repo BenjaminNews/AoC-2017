4537f2a67b4600cf9e131832eed9146f87f963f9
Kane <benjamin.kane@sky.uk>
2018-03-28 14:38:25 +0100

    adding 2016 day one

--- src/main/java/AdventOfCode2016/AdventOfCodeDayOne.java
+++ src/main/java/AdventOfCode2016/AdventOfCodeDayOne.java
@@ -8,7 +8,6 @@ public class AdventOfCodeDayOne {
 
     private int xCor = 0;
     private int yCor = 0;
-    private int firstVisitedTwiceDistance = -1;
 
     private ArrayList<String> visitedCoordinates = new ArrayList<>();
 
@@ -26,34 +25,27 @@ public class AdventOfCodeDayOne {
         for(String s: moves) {
             String direction = s.substring(0,1);
             int distance = Integer.parseInt(s.substring(1, s.length()));
-
             if(direction.equals("L")) {
                 moveLeft(distance);
             } else if(direction.equals("R")) {
                 moveRight(distance);
             }
         }
-        System.out.println("Distance from start: " + calculateDistance() + " xcor: " + xCor + " ycor: " + yCor);
         return calculateDistance();
     }
 
     public int getDistanceFromAlreadyVisitedPosition() {
-
         for(String s: moves) {
             if(encounteredSameLocation == false) {
                 String direction = s.substring(0, 1);
                 int distance = Integer.parseInt(s.substring(1, s.length()));
-
                 if (direction.equals("L")) {
                     moveLeftTwo(distance);
                 } else if (direction.equals("R")) {
                     moveRightTwo(distance);
                 }
-            } else {
-                break;
             }
         }
-        System.out.println("Distance from start: " + calculateDistance() + " xcor: " + xCor + " ycor: " + yCor);
         return calculateDistance();
     }
 
@@ -85,61 +77,70 @@ public class AdventOfCodeDayOne {
         if(northFacing) {
             eastFacing = true;
             northFacing = false;
-            for(int i = distance; i > 0; i--) {
-                xCor++;
-                String currentPosition = xCor + "," + yCor;
-                System.out.println("current position: " + currentPosition);
-                if(visitedCoordinates.contains(currentPosition)) {
-                    encounteredSameLocation = true;
-                    break;
-                } else {
-                    visitedCoordinates.add(currentPosition);
-                }
-            }
+            moveEast(distance);
         } else if(eastFacing) {
             southFacing = true;
             eastFacing = false;
-            for(int i = distance; i > 0; i--) {
-                yCor--;
-                String currentPosition = xCor + "," + yCor;
-                System.out.println("current position: " + currentPosition);
-
-                if(visitedCoordinates.contains(currentPosition)) {
-                    encounteredSameLocation = true;
-                    break;
-                } else {
-                    visitedCoordinates.add(currentPosition);
-                }
-            }
+            MoveSouth(distance);
         } else if(southFacing) {
             southFacing = false;
             westFacing = true;
-            for(int i = distance; i > 0; i--) {
-                xCor--;
-                String currentPosition = xCor + "," + yCor;
-                System.out.println("current position: " + currentPosition);
-
-                if(visitedCoordinates.contains(currentPosition)) {
-                    encounteredSameLocation = true;
-                    break;
-                } else {
-                    visitedCoordinates.add(currentPosition);
-                }
-            }
+            moveWest(distance);
         } else if(westFacing) {
             westFacing = false;
             northFacing = true;
-            for(int i = distance; i > 0; i--) {
-                yCor++;
-                String currentPosition = xCor + "," + yCor;
-                System.out.println("current position: " + currentPosition);
+            moveNorth(distance);
+        }
+    }
 
-                if(visitedCoordinates.contains(currentPosition)) {
-                    encounteredSameLocation = true;
-                    break;
-                } else {
-                    visitedCoordinates.add(currentPosition);
-                }
+    private void moveNorth(int distance) {
+        for(int i = distance; i > 0; i--) {
+            yCor++;
+            String currentPosition = xCor + "," + yCor;
+            if(visitedCoordinates.contains(currentPosition)) {
+                encounteredSameLocation = true;
+                break;
+            } else {
+                visitedCoordinates.add(currentPosition);
+            }
+        }
+    }
+
+    private void moveWest(int distance) {
+        for(int i = distance; i > 0; i--) {
+            xCor--;
+            String currentPosition = xCor + "," + yCor;
+            if(visitedCoordinates.contains(currentPosition)) {
+                encounteredSameLocation = true;
+                break;
+            } else {
+                visitedCoordinates.add(currentPosition);
+            }
+        }
+    }
+
+    private void MoveSouth(int distance) {
+        for(int i = distance; i > 0; i--) {
+            yCor--;
+            String currentPosition = xCor + "," + yCor;
+            if(visitedCoordinates.contains(currentPosition)) {
+                encounteredSameLocation = true;
+                break;
+            } else {
+                visitedCoordinates.add(currentPosition);
+            }
+        }
+    }
+
+    private void moveEast(int distance) {
+        for(int i = distance; i > 0; i--) {
+            xCor++;
+            String currentPosition = xCor + "," + yCor;
+            if(visitedCoordinates.contains(currentPosition)) {
+                encounteredSameLocation = true;
+                break;
+            } else {
+                visitedCoordinates.add(currentPosition);
             }
         }
     }
@@ -148,62 +149,30 @@ public class AdventOfCodeDayOne {
         if(northFacing) {
             westFacing = true;
             northFacing = false;
-            for(int i = distance; i > 0; i--) {
-                xCor--;
-                String currentPosition = xCor + "," + yCor;
-                System.out.println("current position: " + currentPosition);
-
-                if(visitedCoordinates.contains(currentPosition)) {
-                    encounteredSameLocation = true;
-                    break;
-                } else {
-                    visitedCoordinates.add(currentPosition);
-                }
-            }
+            moveWest(distance);
         } else if(eastFacing) {
             northFacing = true;
             eastFacing = false;
-            for(int i = distance; i > 0; i--) {
-                yCor++;
-                String currentPosition = xCor + "," + yCor;
-                System.out.println("current position: " + currentPosition);
-
-                if(visitedCoordinates.contains(currentPosition)) {
-                    encounteredSameLocation = true;
-                    break;
-                } else {
-                    visitedCoordinates.add(currentPosition);
-                }
-            }
+            moveNorth(distance);
         } else if(southFacing) {
             southFacing = false;
             eastFacing = true;
-            for(int i = distance; i > 0; i--) {
-                xCor++;
-                String currentPosition = xCor + "," + yCor;
-                System.out.println("current position: " + currentPosition);
-
-                if(visitedCoordinates.contains(currentPosition)) {
-                    encounteredSameLocation = true;
-                    break;
-                } else {
-                    visitedCoordinates.add(currentPosition);
-                }
-            }
+            moveEast(distance);
         } else if(westFacing) {
             westFacing = false;
             southFacing = true;
-            for(int i = distance; i > 0; i--) {
-                yCor--;
-                String currentPosition = xCor + "," + yCor;
-                System.out.println("current position: " + currentPosition);
+            moveSouth(distance);
+        }
+    }
 
-                if(visitedCoordinates.contains(currentPosition)) {
-                    encounteredSameLocation = true;
-                    break;
-                } else {
-                    visitedCoordinates.add(currentPosition);
-                }
+    private void moveSouth(int distance) {
+        for(int i = distance; i > 0; i--) {
+            yCor--;
+            String currentPosition = xCor + "," + yCor;
+            if(visitedCoordinates.contains(currentPosition)) {
+                encounteredSameLocation = true;
+            } else {
+                visitedCoordinates.add(currentPosition);
             }
         }
     }
@@ -228,12 +197,11 @@ public class AdventOfCodeDayOne {
         }
     }
 
-
-
     public static void main(String[] args) {
         AdventOfCodeDayOne adventOfCodeDayOne = new AdventOfCodeDayOne();
         adventOfCodeDayOne.setup(input);
-        adventOfCodeDayOne.getDistanceFromEndPoint();
+        int distanceFromStart = adventOfCodeDayOne.getDistanceFromEndPoint();
+        System.out.println("distance from start: " + distanceFromStart);
 
         adventOfCodeDayOne = new AdventOfCodeDayOne();
         adventOfCodeDayOne.setup(input);
@@ -242,8 +210,4 @@ public class AdventOfCodeDayOne {
     }
 
     private static final String input = "R4, R5, L5, L5, L3, R2, R1, R1, L5, R5, R2, L1, L3, L4, R3, L1, L1, R2, R3, R3, R1, L3, L5, R3, R1, L1, R1, R2, L1, L4, L5, R4, R2, L192, R5, L2, R53, R1, L5, R73, R5, L5, R186, L3, L2, R1, R3, L3, L3, R1, L4, L2, R3, L5, R4, R3, R1, L1, R5, R2, R1, R1, R1, R3, R2, L1, R5, R1, L5, R2, L2, L4, R3, L1, R4, L5, R4, R3, L5, L3, R4, R2, L5, L5, R2, R3, R5, R4, R2, R1, L1, L5, L2, L3, L4, L5, L4, L5, L1, R3, R4, R5, R3, L5, L4, L3, L1, L4, R2, R5, R5, R4, L2, L4, R3, R1, L2, R5, L5, R1, R1, L1, L5, L5, L2, L1, R5, R2, L4, L1, R4, R3, L3, R1, R5, L1, L4, R2, L3, R5, R3, R1, L3";
-
-    public int getFirstVisitedTwiceLocation() {
-        return this.firstVisitedTwiceDistance;
-    }
 }
