dcb9ab3972c2398b5545d7379ed37f39e9430ac1
Kane <benjamin.kane@sky.uk>
2018-04-08 16:38:23 +0100

    adding day twelve with part one passing with tests and part two tests passing but returning wrong result

--- src/test/java/AdventOfCode2015/AdventOfCodeDayTwelveTest.java
+++ src/test/java/AdventOfCode2015/AdventOfCodeDayTwelveTest.java
@@ -76,9 +76,53 @@ public class AdventOfCodeDayTwelveTest {
         int result = 0;
 
         for(String s : input.split("\n")) {
-            result += adventOfCodeDayTwelve.getScore(input);
+            result += adventOfCodeDayTwelve.getScore(s);
 
         }
         assertEquals(0, result);
+    }
+
+    @Test
+    public void inputReturnsAScoreOSix() {
+
+        AdventOfCodeDayTwelve adventOfCodeDayTwelve = new AdventOfCodeDayTwelve();
+
+        String input = "[1,2,3]";
+
+        int result = adventOfCodeDayTwelve.getScorePartTwo(input);
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void inputReturnsAScoreOfZero() {
+
+        AdventOfCodeDayTwelve adventOfCodeDayTwelve = new AdventOfCodeDayTwelve();
+
+        String input = "[1,{\"c\":\"red\",\"b\":2},3]";
+
+        int result = adventOfCodeDayTwelve.getScorePartTwo(input);
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void inputReturnsAScoreOfZeroSecondTime() {
+
+        AdventOfCodeDayTwelve adventOfCodeDayTwelve = new AdventOfCodeDayTwelve();
+
+        String input = "{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}";
+
+        int result = adventOfCodeDayTwelve.getScorePartTwo(input);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void inputReturnsAScoreOfSix() {
+
+        AdventOfCodeDayTwelve adventOfCodeDayTwelve = new AdventOfCodeDayTwelve();
+
+        String input = "[1,\"red\",5]";
+
+        int result = adventOfCodeDayTwelve.getScorePartTwo(input);
+        assertEquals(6, result);
     }
 }
