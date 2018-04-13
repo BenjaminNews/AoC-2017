d56d073e4b95a56176adc127e140abbbca8e32d3
Kane <benjamin.kane@sky.uk>
2018-04-13 14:24:02 +0100

    adding complete day 11 with tests

--- src/test/java/AdventOfCode2016/AdventOfCodeDayFourTest.java
+++ src/test/java/AdventOfCode2016/AdventOfCodeDayFourTest.java
@@ -1,21 +1,5 @@
-//package AdventOfCode2016;
-//
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class AdventOfCodeDayFourTest {
-//
-//    @Test
-//    public void inputOneIsARealRoom() {
-//        AdventOfCodeDayFour adventOfCodeDayFour = new AdventOfCodeDayFour();
-//
-//        String input = "aaaaa-bbb-x-y-z-123[abxyz]";
-//
-//        adventOfCodeDayFour.setUp(input);
-//
-//        int result = adventOfCodeDayFour.getValidCount();
-//
-//        assertEquals(1, result);
-//    }
-//}
+import static org.junit.Assert.*;
+
+public class AdventOfCodeDayFourTest {
+
+}
--- src/test/java/AdventOfCode2017/AdventOfCodeDayElevenTest.java
+++ src/test/java/AdventOfCode2017/AdventOfCodeDayElevenTest.java
@@ -1,246 +1,52 @@
 package AdventOfCode2017;
 
-import AdcentOfCode2017.AdventOfCodeDayEleven;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class AdventOfCodeDayElevenTest {
 
     @Test
-    public void nNisTwoStepsAway() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "n,n";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-        assertEquals(2, result);
-    }
-
-    @Test
-    public void sSisTwoStepsAway() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "s,s";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-        assertEquals(2, result);
-    }
-
-    @Test
-    public void nwNELeaveOneStepsAway() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "nw,ne";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-        assertEquals(1, result);
-    }
-
-    @Test
-    public void neNWLeaveOneStepsAway() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "ne,nw";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-        assertEquals(1, result);
-    }
-
-    @Test
-    public void neNWNwLeaveTwoStepsAway() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "ne,nw,nw";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-        assertEquals(2, result);
-    }
-
-    @Test
-    public void nwNENELeaveTwoStepsAway() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "nw,ne,ne";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
-        assertEquals(2, result);
-    }
-
-    @Test
-    public void sSNLeavesOneStepsAway() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "s,s,n";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
-        assertEquals(1, result);
-    }
-
-    @Test
-    public void nNSLeavesOneStepsAway() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "n,n,s";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
-        assertEquals(1, result);
-    }
-
-    @Test
-    public void neNeNeLeavesThreeStepsFromStart() {
+    public void inputOneisThreeStepsAway() {
         AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
 
         String input = "ne,ne,ne";
 
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
+        adventOfCodeDayEleven.performSteps(input);
+        int result = adventOfCodeDayEleven.getDistance();
         assertEquals(3, result);
     }
 
     @Test
-    public void neNeSwSwReturnsToStartingPoint() {
+    public void inputTwoReturnsToStart() {
         AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
 
         String input = "ne,ne,sw,sw";
 
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
+        adventOfCodeDayEleven.performSteps(input);
+        int result = adventOfCodeDayEleven.getDistance();
         assertEquals(0, result);
     }
 
     @Test
-    public void neNeSSLeavesThwoStepsFromStart() {
+    public void inputThreeIsTwoStepsAway() {
         AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
 
         String input = "ne,ne,s,s";
 
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
+        adventOfCodeDayEleven.performSteps(input);
+        int result = adventOfCodeDayEleven.getDistance();
         assertEquals(2, result);
     }
 
     @Test
-    public void nwNwSSLeavesThwoStepsFromStart() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "nw,nw,s,s";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
-        assertEquals(2, result);
-    }
-
-    @Test
-    public void seSwSeSwLeaveTwoStepsFromStart() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "se,sw,se,sw";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
-        assertEquals(2, result);
-    }
-
-    @Test
-    public void seSwSeSWSWLeavesThreeStepsFromStart() {
+    public void inputFourIsThreeStepsAway() {
         AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
 
         String input = "se,sw,se,sw,sw";
 
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
+        adventOfCodeDayEleven.performSteps(input);
+        int result = adventOfCodeDayEleven.getDistance();
         assertEquals(3, result);
     }
-
-    @Test
-    public void seSeNwNwReturnsToZero() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "se,se,nw,nw";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
-        assertEquals(0, result);
-    }
-
-    @Test
-    public void neNNLeavesThreeStepsFromStart() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "ne,n,n";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
-        assertEquals(3, result);
-    }
-
-    @Test
-    public void seSSLeavesThreeStepsFromStart() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "se,s,s";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
-        assertEquals(3, result);
-    }
-
-    @Test
-    public void seSwSOneStepsFromStart() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "se,sw";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
-        assertEquals(1, result);
-    }
-
-    @Test
-    public void swSeSOneStepsFromStart() {
-        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-
-        String input = "sw,se";
-
-        int result = adventOfCodeDayEleven.setEndPosition(input);
-
-        assertEquals(1, result);
-    }
-//
-//    @Test
-//    public void swSeSwSeSeLeavesTwoStepsFromStart() {
-//        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-//
-//        String input = "sw,se,sw";
-//
-//        int result = adventOfCodeDayEleven.setEndPosition(input);
-//
-//        assertEquals(2, result);
-//    }
-//
-//    @Test
-//    public void swSeSwSeLeavesTwoStepsFromStart() {
-//        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-//
-//        String input = "sw,se,sw,se";
-//
-//        int result = adventOfCodeDayEleven.setEndPosition(input);
-//
-//        assertEquals(2, result);
-//    }
-//
-//    @Test
-//    public void swSeSwSeSeLeavesThreeStepsFromStart() {
-//        AdventOfCodeDayEleven adventOfCodeDayEleven = new AdventOfCodeDayEleven();
-//
-//        String input = "sw,se,sw,se,se";
-//
-//        int result = adventOfCodeDayEleven.setEndPosition(input);
-//
-//        assertEquals(3, result);
-//    }
 }
