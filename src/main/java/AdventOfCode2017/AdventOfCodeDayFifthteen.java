169e94742f2d890c88a608bfd6fb132a00dff764
Kane <benjamin.kane@sky.uk>
2018-04-17 16:12:54 +0100

    refactoring, implmeneting and updating tests and adding part two

--- src/main/java/AdventOfCode2017/AdventOfCodeDayFifthteen.java
+++ src/main/java/AdventOfCode2017/AdventOfCodeDayFifthteen.java
@@ -1,13 +1,11 @@
 package AdventOfCode2017;
 
-import java.math.BigInteger;
-
 public class AdventOfCodeDayFifthteen {
 
-    private long generatorAStartingValue;
-    private long generatorBStartingValue;
-    private long generatorAFactor;
-    private long generatorBFactor;
+    private int generatorAStartingValue;
+    private int generatorBStartingValue;
+    private int generatorAFactor;
+    private int generatorBFactor;
 
     private int steps = 0;
     private int matches = 0;
@@ -22,71 +20,62 @@ public class AdventOfCodeDayFifthteen {
         this.steps = steps;
     }
 
-    public int getSteps() {
-        return this.steps;
-    }
-
-    private void updateGeneratorValues() {
-        generatorAStartingValue = ((generatorAStartingValue * generatorAFactor) % 2147483647);
-
-        String generatorAInBinary = String.format("%032d", new BigInteger(Long.toBinaryString((long)generatorAStartingValue)));
-
-        generatorBStartingValue = ((generatorBStartingValue * generatorBFactor) % 2147483647);
-
-        String generatorBInBinary = String.format("%032d", new BigInteger(Long.toBinaryString((long)generatorBStartingValue)));
-
-        String genaratorASixteenBits = generatorAInBinary.substring(16);
-        String generatorBSixteenBits = generatorBInBinary.substring(16);
-
-        if (genaratorASixteenBits.equals(generatorBSixteenBits)) {
-            matches++;
-            System.out.println("matches: " + matches);
+    private void updateGeneratorValuesPartOne() {
+        long p1 = generatorAStartingValue;
+        long p2 = generatorBStartingValue;
+        int c = 0;
+        for(int i = 0; i < steps; i++) {
+            p1 = (p1 * generatorAFactor) % 2147483647;
+            p2 = (p2 * generatorBFactor) % 2147483647;
+            if ((p1 & 65535) == (p2 & 65535))
+                c++;
         }
+        matches = c;
     }
 
-    private void updateGeneratorValuesTwo(int i) {
-        generatorAStartingValue = ((generatorAStartingValue * generatorAFactor) % 2147483647);
+    public void updateGeneratorValuesPartTwo() {
 
-        String generatorAInBinary = String.format("%032d", new BigInteger(Long.toBinaryString((long)generatorAStartingValue)));
+        long p1 = generatorAStartingValue;
+        long p2 = generatorBStartingValue;
+        int c = 0;
+        for (int i = 0; i < steps; i++) {
+            do {
+                p1 = (p1 * generatorAFactor) % 2147483647;
+            } while (p1 % 4 != 0);
+            do {
+                p2 = (p2 * generatorBFactor) % 2147483647;
+            } while (p2 % 8 != 0);
 
-        generatorBStartingValue = ((generatorBStartingValue * generatorBFactor) % 2147483647);
-
-        String generatorBInBinary = String.format("%032d", new BigInteger(Long.toBinaryString((long)generatorBStartingValue)));
-
-        String generatorASixteenBits = generatorAInBinary.substring(16, generatorAInBinary.length());
-        String generatorBSixteenBits = generatorBInBinary.substring(16, generatorBInBinary.length());
-
-        if ((generatorAStartingValue % 4 == 0) && (generatorBStartingValue % 8 == 0)) {
-            if(Integer.parseInt(generatorASixteenBits, 2) % 4 == 0) {
-                System.out.println("generator a value parsed from binary divisible by four: " + Integer.parseInt(generatorASixteenBits, 2));
-                if(Integer.parseInt(generatorBSixteenBits, 2) % 8 == 0) {
-                    System.out.println("generator b value parsed from binary divisible by eight: " + Integer.parseInt(generatorBSixteenBits, 2));
-                    if (generatorASixteenBits.equals(generatorBSixteenBits)) {
-                        matches++;
-                        System.out.println("matches: " + matches);
-                    }
-                }
+            if ((p1 & 65535) == (p2 & 65535)) {
+                c++;
             }
-
         }
+        matches = c;
     }
 
     public int getMatches() {
         return matches;
     }
 
-    public static void main(String[] args) {
-
-        AdventOfCodeDayFifthteen adventOfCodeDayFifthteen = new AdventOfCodeDayFifthteen(783, 16807, 325, 48271, 5000000);
-
-        adventOfCodeDayFifthteen.performSteps(adventOfCodeDayFifthteen.getSteps());
-        System.out.println("result: " + adventOfCodeDayFifthteen.getMatches());
+    public void performStepsPartOne() {
+        for(int i = 0; i < steps; i++ ) {
+            updateGeneratorValuesPartOne();
+        }
     }
 
-    public void performSteps(int stepCount) {
-        for(int i = 0; i < stepCount; i++ ) {
-            //updateGeneratorValues();
-            updateGeneratorValuesTwo(i);
+    public void performStepsPartTwo() {
+        for(int i = 0; i < steps; i++ ) {
+            updateGeneratorValuesPartTwo();
         }
+    }
+
+    public static void main(String[] args) {
+        AdventOfCodeDayFifthteen adventOfCodeDayFifthteen = new AdventOfCodeDayFifthteen(783, 16807, 325, 48271, 40000000);
+        adventOfCodeDayFifthteen.performStepsPartOne();
+        System.out.println("result: " + adventOfCodeDayFifthteen.getMatches());
+
+        adventOfCodeDayFifthteen = new AdventOfCodeDayFifthteen(783, 16807, 325, 48271, 5000000);
+        adventOfCodeDayFifthteen.performStepsPartTwo();
+        System.out.println("result: " + adventOfCodeDayFifthteen.getMatches());
     }
 }
--- src/test/java/AdventOfCode2017/AdventOfCodeDayFifthteenTest.java
+++ src/test/java/AdventOfCode2017/AdventOfCodeDayFifthteenTest.java
@@ -1,5 +1,41 @@
 package AdventOfCode2017;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class AdventOfCodeDayFifthteenTest {
 
+    @Test
+    public void inputReturnsOneMatch() {
+        AdventOfCodeDayFifthteen adventOfCodeDayFifthteen = new AdventOfCodeDayFifthteen(65, 16807, 8921, 48271, 5);
+
+        adventOfCodeDayFifthteen.performStepsPartOne();
+        assertEquals(1, adventOfCodeDayFifthteen.getMatches());
+    }
+
+    @Test
+    public void inputReturnsFiveHundredAndEightyEightMatches() {
+        AdventOfCodeDayFifthteen adventOfCodeDayFifthteen = new AdventOfCodeDayFifthteen(65, 16807, 8921, 48271, 40000000);
+
+        adventOfCodeDayFifthteen.performStepsPartOne();
+        assertEquals(588, adventOfCodeDayFifthteen.getMatches());
+    }
+
+    @Test
+    public void inputReturnsOneMatchPartTwo() {
+        AdventOfCodeDayFifthteen adventOfCodeDayFifthteen = new AdventOfCodeDayFifthteen(65, 16807, 8921, 48271, 1086);
+
+        adventOfCodeDayFifthteen.performStepsPartTwo();
+        assertEquals(1, adventOfCodeDayFifthteen.getMatches());
+    }
+
+    @Test
+    public void inputReturnsThreeHundredAndNinePairs() {
+        AdventOfCodeDayFifthteen adventOfCodeDayFifthteen = new AdventOfCodeDayFifthteen(65, 16807, 8921, 48271, 5000000);
+
+        adventOfCodeDayFifthteen.performStepsPartTwo();
+        int result = adventOfCodeDayFifthteen.getMatches();
+        assertEquals(309, result);
+    }
 }
