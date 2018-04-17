693b756d62236b3bf89077e5e0c74aa243464da5
Kane <benjamin.kane@sky.uk>
2018-04-17 13:21:28 +0100

    adding day eighteen with passing tests but not passing challenge

--- src/main/java/AdventOfCode2017/AdventOfCodeDayEighteen.java
+++ src/main/java/AdventOfCode2017/AdventOfCodeDayEighteen.java
@@ -1,193 +1,157 @@
-package AdcentOfCode2017;
+package AdventOfCode2017;
 
 import java.util.HashMap;
 
 public class AdventOfCodeDayEighteen {
 
-    private HashMap<String, Integer> notes = new HashMap<String, Integer>();
-    private HashMap<String, Integer> notesPlayed = new HashMap<String, Integer>();
+    private int lastPlayedSound = -1;
 
-    private int instructionPosition = 0;
+    private HashMap<String, Integer> registerValues = new HashMap<String, Integer>();
 
-    private String lastPlayedSound;
+    private static int currentPosition = 0;
 
-    private void setup() {
-        notes.put("i", 0);
-        notes.put("a", 0);
-        notes.put("p", 0);
-        notes.put("b", 0);
-        notes.put("f", 0);
-        notes.put("1", 0);
+    public void setUp() {
+        String[] registerNames = new String[]{ "i", "a", "p", "b", "f", "i" };
+        for(String s: registerNames) { registerValues.put(s, 0); }
     }
 
-    public int getRecovery(String input) {
-
-        String[] commands = input.split("\n");
-
-        for (int i = 0; i < commands.length; i++) {
-            String[] commandInstructions = commands[i].split(" ");
-
-            String instruction = commandInstructions[0];
-            String key = commandInstructions[1];
-            System.out.println(instruction);
-            if(instruction.equals("set")) {
-                set(commandInstructions[2], key);
-            } else if(instruction.equals("add")) {
-                add(commandInstructions[2], key);
-            } else if(instruction.equals("mul")) {
-                multiply(commandInstructions[2], key);
-            } else if(instruction.equals("mod")) {
-                mod(commandInstructions[2], key);
-            } else if(instruction.equals("jgz")) {
-                jgz(commandInstructions[2], key);
-            } else if(instruction.equals("snd")) {
-                snd(key);
-            } else if(commands.equals("rcv")) {
-                rcv();
-            }
-        }
-
-        return notesPlayed.get(lastPlayedSound);
+    public void set(String x, String y) {
+        registerValues.put(x, getValueofKey(y));
+        currentPosition++;
     }
 
-    private void rcv() {
-        if(notesPlayed.containsKey(lastPlayedSound)) {
-            if(notesPlayed.get(lastPlayedSound) != 0) {
-                System.out.println("last played sound: " + lastPlayedSound + " " + notesPlayed.get(lastPlayedSound));
-            }
+    public void add(String x, String y) {
+        registerValues.put(x, getValueofKey(x) + getValueofKey(y));
+        currentPosition++;
+    }
+
+    public void mul(String x, String y) {
+        registerValues.put(x, getValueofKey(x) * getValueofKey(y));
+        currentPosition++;
+    }
+
+    public void mod(String x, String y) {
+        registerValues.put(x, getValueofKey(x) % getValueofKey(y));
+        currentPosition++;
+    }
+
+    public void jgz(String x, String y) {
+        if(getValueofKey(x) > 0) {
+            currentPosition += getValueofKey(y);
+        } else {
+            currentPosition++;
         }
     }
 
-    private void snd(String key) {
-        System.out.println("playing sound of (" + key + "): " + notes.get(key));
-        notesPlayed.put(key, notes.get(key));
-        lastPlayedSound = key;
+    public void snd(String x) {
+        lastPlayedSound = registerValues.get(x);
+        System.out.println("playing: " + lastPlayedSound);
+        currentPosition++;
     }
 
-    private void jgz(String commandInstruction, String key) {
-        int updateValueOne = notes.get(key);
-        if(updateValueOne > 0) {
-            int newSetValue;
-            if (isInt(commandInstruction)) {
-                newSetValue = Integer.parseInt(commandInstruction);
+    public int rcv(String x) {
+        System.out.println("recovering");
+        if(getValueofKey(x) != 0) {
+            lastPlayedSound = getValueofKey(x);
+        }
+        currentPosition++;
+        return lastPlayedSound;
+    }
+
+    private int getValueofKey(String checkValue) {
+        try {
+            return(Integer.parseInt(checkValue));
+        } catch(Exception e) {
+            return(registerValues.get(checkValue));
+        }
+    }
+
+    private void performAction(String action) {
+        if(action.startsWith("snd")) {
+            snd(action.split(" ")[1]);
+        } else if(action.startsWith("set")) {
+            set(action.split(" ")[1], action.split(" ")[2]);
+        } else if(action.startsWith("add")) {
+            add(action.split(" ")[1], action.split(" ")[2]);
+        } else if(action.startsWith("mul")) {
+            mul(action.split(" ")[1], action.split(" ")[2]);
+        } else if(action.startsWith("mod")) {
+            mod(action.split(" ")[1], action.split(" ")[2]);
+        } else if(action.startsWith("rcv")) {
+            rcv(action.split(" ")[1]);
+        } else if(action.startsWith("jgz")) {
+            jgz(action.split(" ")[1], action.split(" ")[2]);
+        }
+    }
+
+    public int getRecovery() {
+        return lastPlayedSound;
+    }
+
+    public int performActions(String actions) {
+        String[] actionsToPerform = actions.split("\n");
+
+        while(currentPosition < actionsToPerform.length) {
+            System.out.println("instruction: " + actionsToPerform[currentPosition]);
+            if(actionsToPerform[currentPosition].startsWith("rcv")) {
+                return rcv(actionsToPerform[currentPosition].split(" ")[1]);
             } else {
-                newSetValue = notes.get(key);
+                String instruction = actionsToPerform[currentPosition];
+                performAction(instruction);
             }
-            instructionPosition += updateValueOne + newSetValue;
-        } else {
-            instructionPosition++;
         }
-    }
-
-    private void mod(String commandInstruction, String key) {
-        int updateValueOne = notes.get(key);
-        int newSetValue;
-        if(isInt(commandInstruction)) {
-            newSetValue = Integer.parseInt(commandInstruction);
-        } else {
-            newSetValue = notes.get(key);
-        }
-        if(updateValueOne != 0) {
-            notes.replace(key, updateValueOne % newSetValue);
-        }
-        instructionPosition++;
-    }
-
-    private void multiply(String commandInstruction, String key) {
-        int updateValueOne = notes.get(key);
-        int newSetValue;
-        if(isInt(commandInstruction)) {
-            newSetValue = Integer.parseInt(commandInstruction);
-        } else {
-            newSetValue = notes.get(key);
-        }
-        notes.replace(key, updateValueOne * newSetValue);
-        instructionPosition++;
-    }
-
-    private void add(String commandInstruction, String key) {
-        int updateValueOne = notes.get(key);
-        int newSetValue;
-        if(isInt(commandInstruction)) {
-            newSetValue = Integer.parseInt(commandInstruction);
-        } else {
-            newSetValue = notes.get(key);
-        }
-        notes.replace(key, updateValueOne * newSetValue);
-        instructionPosition++;
-    }
-
-    private void set(String commandInstruction, String key) {
-        String updateValue = commandInstruction;
-        int newSetValue = 0;
-        if(isInt(updateValue)) {
-            newSetValue = Integer.parseInt(updateValue);
-        } else {
-            newSetValue = notes.get(key);
-        }
-        if(notes.containsKey(key)) {
-            notes.replace(key, newSetValue);
-        } else {
-            notes.put(key, newSetValue);
-        }
-        instructionPosition++;
-    }
-
-    private boolean isInt(String s) {
-        try{
-            Integer.parseInt(s);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
+        return -1;
     }
 
     public static void main(String[] args) {
         AdventOfCodeDayEighteen adventOfCodeDayEighteen = new AdventOfCodeDayEighteen();
-        adventOfCodeDayEighteen.setup();
-        int result = adventOfCodeDayEighteen.getRecovery(input);
+        adventOfCodeDayEighteen.setUp();
+
+        adventOfCodeDayEighteen.performActions(input);
+        int result = adventOfCodeDayEighteen.getRecovery();
+        System.out.println("last played sound: " + result);
     }
 
-    private static String input = "set i 31\n" +
-            "set a 1\n" +
-            "mul p 17\n" +
-            "jgz p p\n" +
-            "mul a 2\n" +
-            "add i -1\n" +
-            "jgz i -2\n" +
-            "add a -1\n" +
-            "set i 127\n" +
-            "set p 735\n" +
-            "mul p 8505\n" +
-            "mod p a\n" +
-            "mul p 129749\n" +
-            "add p 12345\n" +
-            "mod p a\n" +
-            "set b p\n" +
-            "mod b 10000\n" +
-            "snd b\n" +
-            "add i -1\n" +
-            "jgz i -9\n" +
-            "jgz a 3\n" +
-            "rcv b\n" +
-            "jgz b -1\n" +
-            "set f 0\n" +
-            "set i 126\n" +
-            "rcv a\n" +
-            "rcv b\n" +
-            "set p a\n" +
-            "mul p -1\n" +
-            "add p b\n" +
-            "jgz p 4\n" +
-            "snd a\n" +
-            "set a b\n" +
-            "jgz 1 3\n" +
-            "snd b\n" +
-            "set f 1\n" +
-            "add i -1\n" +
-            "jgz i -11\n" +
-            "snd a\n" +
-            "jgz f -16\n" +
-            "jgz a -19";
+    private static String input =  "set i 31\n" +
+                                    "set a 1\n" +
+                                    "mul p 17\n" +
+                                    "jgz p p\n" +
+                                    "mul a 2\n" +
+                                    "add i -1\n" +
+                                    "jgz i -2\n" +
+                                    "add a -1\n" +
+                                    "set i 127\n" +
+                                    "set p 735\n" +
+                                    "mul p 8505\n" +
+                                    "mod p a\n" +
+                                    "mul p 129749\n" +
+                                    "add p 12345\n" +
+                                    "mod p a\n" +
+                                    "set b p\n" +
+                                    "mod b 10000\n" +
+                                    "snd b\n" +
+                                    "add i -1\n" +
+                                    "jgz i -9\n" +
+                                    "jgz a 3\n" +
+                                    "rcv b\n" +
+                                    "jgz b -1\n" +
+                                    "set f 0\n" +
+                                    "set i 126\n" +
+                                    "rcv a\n" +
+                                    "rcv b\n" +
+                                    "set p a\n" +
+                                    "mul p -1\n" +
+                                    "add p b\n" +
+                                    "jgz p 4\n" +
+                                    "snd a\n" +
+                                    "set a b\n" +
+                                    "jgz 1 3\n" +
+                                    "snd b\n" +
+                                    "set f 1\n" +
+                                    "add i -1\n" +
+                                    "jgz i -11\n" +
+                                    "snd a\n" +
+                                    "jgz f -16\n" +
+                                    "jgz a -19";
 }
 
--- src/test/java/AdventOfCode2016/AdventOfCodeDayEighteenTest.java
+++ src/test/java/AdventOfCode2016/AdventOfCodeDayEighteenTest.java
@@ -1,5 +1,20 @@
-import static org.junit.Assert.*;
+package AdventOfCode2016;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class AdventOfCodeDayEighteenTest {
+
+    @Test
+    public void firstRowHasTotalOfFourSafeSpaces() {
+        AdventOfCodeDayEighteen adventOfCodeDayEighteen = new AdventOfCodeDayEighteen();
+
+        String input = "..^^.";
+        adventOfCodeDayEighteen.setUp(input, 100);
+        int result = adventOfCodeDayEighteen.getSafeTileCount(input);
+
+        assertEquals(4, result);
+    }
 
 }
