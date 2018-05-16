bd14eab8daf40b1b4ed31b3301a908b0e1664033
Kane <benjamin.kane@sky.uk>
2018-05-16 09:14:54 +0100

    adding 2016 days five second part. Refactored, cleaned code and updated tests

--- src/main/java/AdventOfCode2016/AdventOfCodeDayFive.java
+++ src/main/java/AdventOfCode2016/AdventOfCodeDayFive.java
@@ -1,4 +1,73 @@
 package AdventOfCode2016;
 
+import java.security.MessageDigest;
+
 public class AdventOfCodeDayFive {
+
+    public String partOne(String data) {
+        String result = "";
+        int x = 0;
+        while (result.length() < 8) {
+            String hash = md5(data + x);
+            if (hash.startsWith("00000")) {
+                result += hash.charAt(5);
+            }
+            x++;
+        }
+        return result;
+    }
+
+    public String partTwo(String data) {
+        String[] result = new String[8];
+        int x = 0;
+        while(hasNullElement(result)) {
+            String hash = md5(data + x);
+            if (hash.startsWith("00000")) {
+                if ((int)hash.charAt(5) > 47 && (int)hash.charAt(5) < 56) {
+                    int position = Integer.parseInt(String.valueOf(hash.charAt(5)));
+                    if (result[position] == null) {
+                        result[position] = String.valueOf(hash.charAt(6));
+                    }
+                }
+            }
+            x++;
+        }
+        return printArray(result);
+    }
+
+    private String printArray(String[] result) {
+        String s = "";
+        for(String s1 : result) {
+            s+= s1;
+        }
+        return s;
+    }
+
+    private boolean hasNullElement(String[] checkValues) {
+        for (String s: checkValues) {
+            if (s == null) { return true; }
+        }
+        return false;
+    }
+
+    public String md5( String input ) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(input.getBytes("UTF-8"));
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; i++) {
+                sb.append(String.format("%02x", array[i]));
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        AdventOfCodeDayFive adventOfCodeDayFive = new AdventOfCodeDayFive();
+        String input = "ojvtpuvg";
+        System.out.println(adventOfCodeDayFive.partOne(input));
+        System.out.println(adventOfCodeDayFive.partTwo(input));
+    }
 }
--- src/test/java/AdventOfCode2016/AdventOfCodeDayFiveTest.java
+++ src/test/java/AdventOfCode2016/AdventOfCodeDayFiveTest.java
@@ -1,14 +1,22 @@
 package AdventOfCode2016;
 
-import AdventOfCode2017.AdventOfCodeDayThirteen;
+import org.junit.Test;
 
-public class AdventOfCodeDayFive {
+import static org.junit.Assert.assertEquals;
 
-    public void valueabc3231929ReturnsHashStartingWithFiveZeros() {
+public class AdventOfCodeDayFiveTest {
+
+    @Test
+    public void partOneTest() {
         AdventOfCodeDayFive adventOfCodeDayFive = new AdventOfCodeDayFive();
+        String result =  adventOfCodeDayFive.partOne("abc");
+        assertEquals("18f47a30", result);
+    }
 
-        String input = "abc";
-        int getHash =  adventOfCodeDayFive.hashCode(input);
-        assertEquals()
+    @Test
+    public void partTwoTest() {
+        AdventOfCodeDayFive adventOfCodeDayFive = new AdventOfCodeDayFive();
+        String result =  adventOfCodeDayFive.partTwo("abc");
+        assertEquals("05ace8e3", result);
     }
 }
