e5565b588e7124c3ddf3f9dd81207c6107877abd
Kane <benjamin.kane@sky.uk>
2018-05-17 17:49:20 +0100

    adding day twenty part one with tests

--- src/test/java/AdventOfCode2016/AdventOfCodeDayTwentyTest.java
+++ src/test/java/AdventOfCode2016/AdventOfCodeDayTwentyTest.java
@@ -1,5 +1,19 @@
-import static org.junit.Assert.*;
+package AdventOfCode2016;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class AdventOfCodeDayTwentyTest {
+    @Test
+    public void lowestIPofThreeIsAvailable() {
+        AdventOfCodeDayTwenty adventOfCodeDayTwenty = new AdventOfCodeDayTwenty();
+        String input = "5-8\n" +
+                       "0-2\n" +
+                       "4-7\n";
+        adventOfCodeDayTwenty.setup(input);
+        double result = adventOfCodeDayTwenty.getLowestAvailableIpAddress();
+        assertEquals(3.0, result, 0.001);
+    }
 
 }
