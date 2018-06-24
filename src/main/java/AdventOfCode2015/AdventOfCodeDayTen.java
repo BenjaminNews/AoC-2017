a571adaed9f107546b80f26f00ff6bb24c6c5f90
Kane <benjamin.kane@sky.uk>
2018-06-24 13:39:38 +0100

    adding day 10

--- src/main/java/AdventOfCode2015/AdventOfCodeDayTen.java
+++ src/main/java/AdventOfCode2015/AdventOfCodeDayTen.java
@@ -1,4 +1,44 @@
 package AdventOfCode2015;
 
 public class AdventOfCodeDayTen {
+
+    public String getStepResult(String input) {
+        String startValue = input;
+        String result = "";
+
+        char[] values = startValue.toCharArray();
+        for (int checkPoint = 0; checkPoint < values.length; checkPoint++) {
+            int count = 1;
+            for (int nextCharPosition = checkPoint + 1; nextCharPosition < values.length; nextCharPosition++) {
+                if (values[checkPoint] == values[nextCharPosition]) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            result += String.valueOf(count) + Character.toString(values[checkPoint]);
+            checkPoint += count - 1;
+        }
+
+        return result;
+    }
+
+    public String performRounds(String input, int count) {
+        for(int i = 0; i < count; i++) {
+            input = getStepResult(input);
+            System.out.println("i: " + i + " " + input);
+        }
+        return input;
+    }
+
+    public static void main(String[] args) {
+        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
+        String input = "1113222113";
+        String result = adventOfCodeDayTen.performRounds(input, 40);
+        System.out.println(result);
+        System.out.println("\n\nlength of result: " + result.length());;
+        result = adventOfCodeDayTen.performRounds(result, 10);
+        System.out.println(result);
+        System.out.println("length of result: " + result.length());
+    }
 }
--- src/test/java/AdventOfCode2015/AdventOfCodeDayTenTest.java
+++ src/test/java/AdventOfCode2015/AdventOfCodeDayTenTest.java
@@ -1,5 +1,74 @@
-import static org.junit.Assert.*;
+package AdventOfCode2015;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class AdventOfCodeDayTenTest {
 
+    @Test
+    public void oneRoundWithStartingInputOfOne() {
+        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
+
+        String input = "1";
+
+        String result = adventOfCodeDayTen.getStepResult(input);
+
+        assertEquals("11", result);
+    }
+
+    @Test
+    public void oneRoundWithStartingInputOfOneOne() {
+        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
+
+        String input = "11";
+
+        String result = adventOfCodeDayTen.getStepResult(input);
+
+        assertEquals("21", result);
+    }
+
+    @Test
+    public void oneRoundWithStartingInputOfTwoOne() {
+        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
+
+        String input = "21";
+
+        String result = adventOfCodeDayTen.getStepResult(input);
+
+        assertEquals("1211", result);
+    }
+
+    @Test
+    public void oneRoundWithStartingInputOfOneTwoOneOne() {
+        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
+
+        String input = "1211";
+
+        String result = adventOfCodeDayTen.getStepResult(input);
+
+        assertEquals("111221", result);
+    }
+
+    @Test
+    public void oneRoundWithStartingInputOfOneOneOneTwoTwoOne() {
+        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
+
+        String input = "111221";
+
+        String result = adventOfCodeDayTen.getStepResult(input);
+
+        assertEquals("312211", result);
+    }
+
+    @Test
+    public void fiveRoundsReturnThreeOneTwoTwoOneOne() {
+        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
+
+        String input = "1";
+
+        String result = adventOfCodeDayTen.performRounds(input, 5);
+
+        assertEquals("312211", result);
+    }
 }
