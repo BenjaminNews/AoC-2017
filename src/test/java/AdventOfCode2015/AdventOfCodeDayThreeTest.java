ac27f07f0e7a9fa7de2da81d5264e98b17ffd90b
Kane <benjamin.kane@sky.uk>
2018-06-24 15:30:07 +0100

    cleaned up test

--- src/test/java/AdventOfCode2015/AdventOfCodeDayThreeTest.java
+++ src/test/java/AdventOfCode2015/AdventOfCodeDayThreeTest.java
@@ -1,75 +1,57 @@
 package AdventOfCode2015;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class AdventOfCodeDayThreeTest {
 
+    private AdventOfCodeDayThree adventOfCodeDayThree;
+    @Before
+    public void setup() {
+        adventOfCodeDayThree = new AdventOfCodeDayThree();
+    }
+
     @Test
     public void inputOneDeliversToTwoHouses() {
-        AdventOfCodeDayThree adventOfCodeDayThree = new AdventOfCodeDayThree();
-
         String input = ">";
-
         int result = adventOfCodeDayThree.deliverPresents(input);
-
         assertEquals(2, result);
     }
 
     @Test
     public void inputTwoDeliversToTwoHouses() {
-        AdventOfCodeDayThree adventOfCodeDayThree = new AdventOfCodeDayThree();
-
         String input = "^>v<";
-
         int result = adventOfCodeDayThree.deliverPresents(input);
-
         assertEquals(4, result);
     }
 
     @Test
     public void inputThreeDeliversToTwoHouses() {
-        AdventOfCodeDayThree adventOfCodeDayThree = new AdventOfCodeDayThree();
-
         String input = "^v^v^v^v^v";
-
         int result = adventOfCodeDayThree.deliverPresents(input);
-
         assertEquals(2, result);
     }
 
     @Test
     public void inputOneDeliversToTwoHousesParttwo() {
-        AdventOfCodeDayThree adventOfCodeDayThree = new AdventOfCodeDayThree();
-
         String input = "^v";
-
         int result = adventOfCodeDayThree.deliverPresentsPartTwo(input);
-
         assertEquals(3, result);
     }
 
     @Test
     public void inputTwoDeliversToTwoHousesPartTwo() {
-        AdventOfCodeDayThree adventOfCodeDayThree = new AdventOfCodeDayThree();
-
         String input = "^>v<";
-
         int result = adventOfCodeDayThree.deliverPresentsPartTwo(input);
-
         assertEquals(3, result);
     }
 
     @Test
     public void inputThreeDeliversToTwoHousesPartTwo() {
-        AdventOfCodeDayThree adventOfCodeDayThree = new AdventOfCodeDayThree();
-
         String input = "^v^v^v^v^v";
-
         int result = adventOfCodeDayThree.deliverPresentsPartTwo(input);
-
         assertEquals(11, result);
     }
-
 }
