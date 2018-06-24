851496bfb1d1ae8d3d5205690b1e211c45fff98d
Kane <benjamin.kane@sky.uk>
2018-06-24 15:35:33 +0100

    cleaned up test

--- src/test/java/AdventOfCode2015/AdventOfCodeDaySixTest.java
+++ src/test/java/AdventOfCode2015/AdventOfCodeDaySixTest.java
@@ -1,58 +1,51 @@
 package AdventOfCode2015;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class AdventOfCodeDaySixTest {
 
+    private AdventOfCodeDaySix adventOfCodeDaySix;
+
+    @Before
+    public void setup() {
+        adventOfCodeDaySix = new AdventOfCodeDaySix();
+    }
+
     @Test
     public void turnOnAllLights() {
-        AdventOfCodeDaySix adventOfCodeDaySix = new AdventOfCodeDaySix();
-
         String input = "turn on 0,0 through 999,999";
-
         int result = adventOfCodeDaySix.performAction(input);
         assertEquals(1000000, result);
     }
 
     @Test
     public void toggleAllLightsOnFirstRow() {
-        AdventOfCodeDaySix adventOfCodeDaySix = new AdventOfCodeDaySix();
-
         String input = "toggle 0,0 through 999,999";
-
         int result = adventOfCodeDaySix.performAction(input);
         assertEquals(1000000, result);
     }
 
     @Test
     public void turnOffMiddleFourLights() {
-        AdventOfCodeDaySix adventOfCodeDaySix = new AdventOfCodeDaySix();
-
         String input = "turn on 0,0 through 999,999\n" +
                        "turn off 499,499 through 500,500";
-
         int result = adventOfCodeDaySix.performAction(input);
         assertEquals(999996, result);
     }
 
     @Test
     public void turnOnBrightness() {
-        AdventOfCodeDaySix adventOfCodeDaySix = new AdventOfCodeDaySix();
-
         String input = "turn on 0,0 through 0,0";
-
         int result = adventOfCodeDaySix.performActionPartTwo(input);
         assertEquals(1, result);
     }
 
     @Test
     public void toggleBrightness() {
-        AdventOfCodeDaySix adventOfCodeDaySix = new AdventOfCodeDaySix();
-
         String input = "toggle 0,0 through 999,999";
-
         int result = adventOfCodeDaySix.performActionPartTwo(input);
         assertEquals(2000000, result);
     }
