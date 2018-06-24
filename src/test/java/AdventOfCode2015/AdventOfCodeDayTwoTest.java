57cea7790a31c9194d1fa1b0143424f8709b066c
Kane <benjamin.kane@sky.uk>
2018-06-24 15:28:16 +0100

    cleaned up test

--- src/test/java/AdventOfCode2015/AdventOfCodeDayTwoTest.java
+++ src/test/java/AdventOfCode2015/AdventOfCodeDayTwoTest.java
@@ -1,55 +1,47 @@
 package AdventOfCode2015;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class AdventOfCodeDayTwoTest {
 
+    private AdventOfCodeDayTwo adventOfCodeDayTwo;
+
+    @Before
+    public void setup() {
+        adventOfCodeDayTwo = new AdventOfCodeDayTwo();
+    }
+
     @Test
     public void inputOneRequiresFiftyEightSquareFeetOfWrappingPaper() {
-        AdventOfCodeDayTwo adventOfCodeDayTwo = new AdventOfCodeDayTwo();
-
         String input = "2x3x4";
-
         adventOfCodeDayTwo.calculateRequiredWrappingPaper(input);
-
         int result = adventOfCodeDayTwo.getWrappingPaperSize();
         assertEquals(58, result);
     }
 
     @Test
     public void inputTwoRequiresFourtyThreeSquareFeetOfWrappingPaper() {
-        AdventOfCodeDayTwo adventOfCodeDayTwo = new AdventOfCodeDayTwo();
-
         String input = "1x1x10";
-
         adventOfCodeDayTwo.calculateRequiredWrappingPaper(input);
-
         int result = adventOfCodeDayTwo.getWrappingPaperSize();
         assertEquals(43, result);
     }
 
     @Test
     public void inputThreeRequiresThirtyFourFeetOfRibbon() {
-        AdventOfCodeDayTwo adventOfCodeDayTwo = new AdventOfCodeDayTwo();
-
         String input = "2x3x4";
-
         adventOfCodeDayTwo.calculateRequiredWrappingPaper(input);
-
         int result = adventOfCodeDayTwo.getBowLenght();
         assertEquals(34, result);
     }
 
     @Test
     public void inputFourRequiresFourteenFeetOfRibbon() {
-        AdventOfCodeDayTwo adventOfCodeDayTwo = new AdventOfCodeDayTwo();
-
         String input = "1x1x10";
-
         adventOfCodeDayTwo.calculateRequiredWrappingPaper(input);
-
         int result = adventOfCodeDayTwo.getBowLenght();
         assertEquals(14, result);
     }
