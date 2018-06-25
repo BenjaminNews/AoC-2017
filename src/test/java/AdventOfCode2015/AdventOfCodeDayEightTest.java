802277c680a9ae31bc04c1818bf853e8d6459561
Kane <benjamin.kane@sky.uk>
2018-06-25 16:09:59 +0100

    cleaned up test

--- src/test/java/AdventOfCode2015/AdventOfCodeDayEightTest.java
+++ src/test/java/AdventOfCode2015/AdventOfCodeDayEightTest.java
@@ -1,5 +1,48 @@
-import static org.junit.Assert.*;
+package AdventOfCode2015;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 public class AdventOfCodeDayEightTest {
 
+    private AdventOfCodeDayEight adventOfCodeDayEight;
+
+    @Before
+    public void setup() {
+         adventOfCodeDayEight = new AdventOfCodeDayEight();
+    }
+
+    @Test
+    public void inputDataReturnsScoreOfTwo() throws IOException {
+        int result = adventOfCodeDayEight.getStringLength("DayEightTestOneInputData.txt");
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void inputDataReturnsScoreOfThree() throws IOException {
+        int result = adventOfCodeDayEight.getStringLength("DayEightTestTwoInputData.txt");
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void inputdataReturnsScoreOfThree() throws IOException {
+        int result = adventOfCodeDayEight.getStringLength("DayEightTestThreeInputData.txt");
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void inputDataReturnsScoreOfFive() throws IOException {
+        int result = adventOfCodeDayEight.getStringLength("DayEightTestFourInputData.txt");
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void inputDataReturnsScoreOfTwelve() throws IOException {
+        int result = adventOfCodeDayEight.getStringLength("DayEightTestFiveInputData.txt");
+        assertEquals(12, result);
+    }
 }
