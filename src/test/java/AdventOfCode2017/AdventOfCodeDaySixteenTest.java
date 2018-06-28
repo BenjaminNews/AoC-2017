b35a2442190dba406c11e5170d25a0c53cd79071
Kane <benjamin.kane@sky.uk>
2018-06-28 17:43:13 +0100

    adding day sixteen with passing tests

--- src/test/java/AdventOfCode2017/AdventOfCodeDaySixteenTest.java
+++ src/test/java/AdventOfCode2017/AdventOfCodeDaySixteenTest.java
@@ -9,219 +9,112 @@ public class AdventOfCodeDaySixteenTest {
     @Test
     public void spinABCDEOfSizeOneReturnsCDEAB() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "s1";
-
-        adventOfCodeDaySixteen.spin(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
-
+        adventOfCodeDaySixteen.setUp("abcde", "s1");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("eabcd", result);
     }
 
     @Test
     public void spinABCDEOfSizeTwoReturnsDEABC() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "s2";
-
-        adventOfCodeDaySixteen.spin(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
-
+        adventOfCodeDaySixteen.setUp("abcde", "s2");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("deabc", result);
     }
 
     @Test
     public void spinABCDEOfSizeThreeReturnsCDEAB() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "s3";
-
-        adventOfCodeDaySixteen.spin(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
-
+        adventOfCodeDaySixteen.setUp("abcde", "s3");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("cdeab", result);
     }
 
     @Test
     public void spinABCDEOfSizeFourReturnsBCDEA() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "s4";
-
-        adventOfCodeDaySixteen.spin(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
-
+        adventOfCodeDaySixteen.setUp("abcde", "s4");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("bcdea", result);
     }
 
     @Test
     public void spinABCDEOfSizeFiveReturnsABCDE() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "s5";
-
-        adventOfCodeDaySixteen.spin(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
-
+        adventOfCodeDaySixteen.setUp("abcde", "s5");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("abcde", result);
     }
 
     @Test
     public void swapFirsttwoCharactersOfABCDEreturnsBACDE() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "x0/1";
-
-        adventOfCodeDaySixteen.swap(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
+        adventOfCodeDaySixteen.setUp("abcde", "x0/1");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("bacde", result);
     }
 
     @Test
     public void swapFirstAndThirdCharactersOfABCDEreturnsCBADE() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "x0/2";
-
-        adventOfCodeDaySixteen.swap(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
+        adventOfCodeDaySixteen.setUp("abcde", "x0/2");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("cbade", result);
     }
 
     @Test
     public void swapThirdAndFourthCharactersOfABCDEreturnsABDCE() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "x2/3";
-
-        adventOfCodeDaySixteen.swap(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
+        adventOfCodeDaySixteen.setUp("abcde", "x2/3");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("abdce", result);
     }
 
     @Test
-    public void swapFourthAndThirdCharactersOfABCDEreturnsABDCE() {
+    public void swapThirdAndSecondCharactersOfABCDEreturnsABDCE() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "x3/2";
-
-        adventOfCodeDaySixteen.swap(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
+        adventOfCodeDaySixteen.setUp("abcde", "x3/2");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("abdce", result);
     }
 
     @Test
     public void swapFifthAndFirstCharactersOfABCDEreturnsEBCDA() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "x4/0";
-
-        adventOfCodeDaySixteen.swap(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
+        adventOfCodeDaySixteen.setUp("abcde", "x4/0");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("ebcda", result);
     }
 
     @Test
     public void swapProgramsAandBReturnsBACDE() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "pa/b";
-
-        adventOfCodeDaySixteen.swapPrograms(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
+        adventOfCodeDaySixteen.setUp("abcde", "pa/b");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("bacde", result);
     }
 
     @Test
     public void swapProgramsEandDReturnsABCED() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "pe/d";
-
-        adventOfCodeDaySixteen.swapPrograms(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
+        adventOfCodeDaySixteen.setUp("abcde", "pe/d");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("abced", result);
     }
 
     @Test
-    public void swapProgramsCandDReturnsABCED() {
+    public void swapProgramsCandDReturnsABDCE() {
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "pc/d";
-
-        adventOfCodeDaySixteen.swapPrograms(command);
-
-        String result = adventOfCodeDaySixteen.getValues();
+        adventOfCodeDaySixteen.setUp("abcde", "pc/d");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("abdce", result);
     }
 
     @Test
     public void allActionsReturnBAEDC(){
         AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "s1\n" +
-                         "x3/4\n" +
-                         "pe/b";
-
-        adventOfCodeDaySixteen.move(command.split("\n"),1);
-
-        String result = adventOfCodeDaySixteen.getValues();
+        adventOfCodeDaySixteen.setUp("abcde", "s1,x3/4,pe/b");
+        String result = adventOfCodeDaySixteen.execute();
         assertEquals("baedc", result);
-    }
-
-    @Test
-    public void allActionsReturnCEADBafterTwoRotations(){
-        AdventOfCodeDaySixteen adventOfCodeDaySixteen = new AdventOfCodeDaySixteen();
-
-        adventOfCodeDaySixteen.setUp("abcde");
-
-        String command = "s1\n" +
-                "x3/4\n" +
-                "pe/b";
-
-        adventOfCodeDaySixteen.move(command.split("\n"),2);
-
-        String result = adventOfCodeDaySixteen.getValues();
-        assertEquals("ceadb", result);
     }
 }
