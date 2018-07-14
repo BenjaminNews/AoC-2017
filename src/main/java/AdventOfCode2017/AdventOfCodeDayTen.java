5d569e657ae851356ef726369f734f970f763620
Kane <benjamin.kane@sky.uk>
2018-07-14 16:29:34 +0100

    adding day ten part two with refactoring, updating tests and made the methods static so can be called against class without instantiation

--- src/main/java/AdventOfCode2017/AdventOfCodeDayTen.java
+++ src/main/java/AdventOfCode2017/AdventOfCodeDayTen.java
@@ -2,81 +2,62 @@ package AdventOfCode2017;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class AdventOfCodeDayTen {
 
-    private int skipSize = 0;
-    private int position = 0;
-
-    private int[] twistString;
-    private int[] steps;
-
-    public void setUp(int length, String input) {
-        twistString = new int[length];
-        for(int i = 0; i < length; i++) {
-            twistString[i] = i;
-        }
-        steps = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
-    }
-
-    public int twist() {
-        for(int twistNumber = 0; twistNumber < steps.length; twistNumber++) {
-            int sizeToTwist = steps[twistNumber];
-            int[] twistArray = getArrayToTwist(sizeToTwist);
-            int[] reversedTwist = twistArrayString(twistArray);
-            updateTwistString(reversedTwist);
-            if(position + sizeToTwist + skipSize >= twistString.length) {
-                position = (sizeToTwist + skipSize + position) - twistString.length;
-            } else {
-                position = position + sizeToTwist + skipSize;
-            }
+    public static int twist(int setSize, String input) {
+        List<Integer> lengths = Arrays.stream(input.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        int[] list = IntStream.iterate(0, n -> n + 1).limit(setSize).toArray();
+        int pos = 0;
+        int skipSize = 0;
+        for (int length : lengths) {
+            spin(list, pos, length);
+            pos += length + skipSize;
             skipSize++;
         }
-        return twistString[0] * twistString[1];
+        return list[0] * list[1];
     }
 
-    private void updateTwistString(int[] reversedTwist) {
-        int currentPosition = position;
-        for(int i = 0; i < reversedTwist.length; i++) {
-            if(currentPosition + 1 > twistString.length) {
-                currentPosition = 0;
-                twistString[currentPosition] = reversedTwist[i];
-            } else {
-                twistString[currentPosition] = reversedTwist[i];
-            }
-            currentPosition++;
-        }
-    }
-
-    private int[] twistArrayString(int[] twistArray) {
-        int[] reversed = new int[twistArray.length];
-        Collections.reverse(Arrays.asList(twistArray));
-        for(int i = twistArray.length - 1, x = 0; i >= 0; i--, x++) {
-            reversed[x] = twistArray[i];
-        }
-        return reversed;
-    }
-
-    private int[] getArrayToTwist(int sizeToTwist) {
-        int index = position;
-        int[] nestedList = new int[sizeToTwist];
-        for(int i = 0; i < sizeToTwist; i++, index++) {
-            if(index + 1 > twistString.length) {
-                index = 0;
-                nestedList[i] = twistString[index];
-            } else {
-                nestedList[i] = twistString[index];
+    public static String twistPartTwo(int setSize, String input, int[] additionalLengths) {
+        List<Integer> lengths = input.chars().boxed().collect(Collectors.toList());
+        Collections.addAll(lengths, Arrays.stream(additionalLengths).boxed().toArray(Integer[]::new));
+        int[] list = IntStream.iterate(0, n -> n + 1).limit(setSize).toArray();
+        int pos = 0;
+        int skipSize = 0;
+        for(int x = 0; x < 64; x++) {
+            for (int length : lengths) {
+                spin(list, pos, length);
+                pos += length + skipSize;
+                skipSize++;
             }
         }
-        return nestedList;
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < list.length; i += 16) {
+            int exclusiveOrValue = list[i] ^ list[i + 1];
+            for(int x = i + 2; x < i + 16; x++) {
+                exclusiveOrValue = exclusiveOrValue ^ list[x];
+            }
+            result.append(Integer.toHexString(exclusiveOrValue).length() == 1 ? "0" + Integer.toHexString(exclusiveOrValue) : Integer.toHexString(exclusiveOrValue));
+        }
+        return result.toString();
+    }
+
+    private static void spin(int[] list, int pos, int length) {
+        for (int i = 0; i < length / 2; i++) {
+            int start = (pos + i) % list.length;
+            int end = (pos + length - 1 - i) % list.length;
+            int tmp = list[start];
+            list[start] = list[end];
+            list[end] = tmp;
+        }
     }
 
     public static void main(String[] args) {
-        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
-
-        adventOfCodeDayTen.setUp(256, input);
-        int result = adventOfCodeDayTen.twist();
-        System.out.println("result: " + result);
+        System.out.println(AdventOfCodeDayTen.twist(256, input));
+        System.out.println(AdventOfCodeDayTen.twistPartTwo(256, input, new int[] {17, 31, 73, 47, 23}));
     }
 
     private static final String input = "192,69,168,160,78,1,166,28,0,83,198,2,254,255,41,12";
--- src/test/java/AdventOfCode2017/AdventOfCodeDayTenTest.java
+++ src/test/java/AdventOfCode2017/AdventOfCodeDayTenTest.java
@@ -7,43 +7,27 @@ import static org.junit.Assert.assertEquals;
 public class AdventOfCodeDayTenTest {
 
     @Test
-    public void inputOneReturnsScoreOfTwo() {
-        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
-        String input = "3";
-
-        adventOfCodeDayTen.setUp(5, input);
-        int result = adventOfCodeDayTen.twist();
-        System.out.println(result);
-        assertEquals(2, result);
+    public void input3_4_1_5ReturnsScoreOfTwelvewithSingleKnotHash() {
+        assertEquals(12, AdventOfCodeDayTen.twist(5, "3,4,1,5"));
     }
 
     @Test
-    public void inputTwoReturnsScoreOfTwelve() {
-        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
-        String input = "3,4";
-
-        adventOfCodeDayTen.setUp(5, input);
-        int result = adventOfCodeDayTen.twist();
-        assertEquals(12, result);
+    public void emptyInputReturnsResultOfa2582a3a0e66e6e86e3812dcb672a272() {
+        assertEquals("a2582a3a0e66e6e86e3812dcb672a272", AdventOfCodeDayTen.twistPartTwo(256, "", new int[] {17, 31, 73, 47, 23}));
     }
 
     @Test
-    public void inputThreeReturnsScoreOfTelve() {
-        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
-        String input = "3,4,1";
-
-        adventOfCodeDayTen.setUp(5, input);
-        int result = adventOfCodeDayTen.twist();
-        assertEquals(12, result);
+    public void inputAoC_2017ReturnsResultOf33efeb34ea91902bb2f59c9920caa6cd() {
+        assertEquals("33efeb34ea91902bb2f59c9920caa6cd", AdventOfCodeDayTen.twistPartTwo(256, "AoC 2017", new int[] {17, 31, 73, 47, 23}));
     }
 
     @Test
-    public void inputFourReturnsScoreOfTwoAfterTwistOfTwelve() {
-        AdventOfCodeDayTen adventOfCodeDayTen = new AdventOfCodeDayTen();
-        String input = "3,4,1,5";
+    public void input1_2_3ReturnsResultOf3efbe78a8d82f29979031a4aa0b16a9d() {
+        assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", AdventOfCodeDayTen.twistPartTwo(256, "1,2,3", new int[] {17, 31, 73, 47, 23}));
+    }
 
-        adventOfCodeDayTen.setUp(5, input);
-        int result = adventOfCodeDayTen.twist();
-        assertEquals(12, result);
+    @Test
+    public void input1_2_4ReturnsResultOf63960835bcdc130f0b66d7ff4f6a5a8e() {
+        assertEquals("63960835bcdc130f0b66d7ff4f6a5a8e", AdventOfCodeDayTen.twistPartTwo(256, "1,2,4", new int[] {17, 31, 73, 47, 23}));
     }
 }
