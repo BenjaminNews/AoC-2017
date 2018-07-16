d042e1f10b65149b156c0e9edaa6d92b5163c92e
Kane <benjamin.kane@sky.uk>
2018-07-16 14:00:32 +0100

    refactored and improved day six

--- src/main/java/AdventOfCode2017/AdventOfCodeDaySix.java
+++ src/main/java/AdventOfCode2017/AdventOfCodeDaySix.java
@@ -2,96 +2,39 @@ package AdventOfCode2017;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AdventOfCodeDaySix {
 
-    private ArrayList<String> visitedPositions = new ArrayList<>();
-
-    private int[] values;
-
-    private int nestedLoopSize = 0;
-
-    public AdventOfCodeDaySix(String input) {
-        if(input.contains("\t")) {
-            values = Arrays.stream(input.split("\t")).mapToInt(Integer::parseInt).toArray();
-        } else {
-            values = Arrays.stream(input.split(", ")).mapToInt(Integer::parseInt).toArray();
-        }
-        visitedPositions.add(getStringRepresentation());
-    }
-
-    public void step() {
-
-        int[] sortedValues = values.clone();
-        Arrays.sort(sortedValues);
-
-        int highestValue = sortedValues[sortedValues.length - 1];
-        int index = getHighestValueIndex(values, highestValue);
-
-        for (int countDownValue = highestValue + 1, startIndex = index; countDownValue > 0; countDownValue--, startIndex++) {
-
-            if (startIndex == index) {
-                //do nothing
+    public int distribute(String input) {
+        ArrayList<String> visitedLocations = new ArrayList<String>();
+        List<Integer> values = Arrays.stream(input.split(", ")).map(Integer::parseInt).collect(Collectors.toList());
+        int cycles = 0;
+        while(true) {
+            int startIndex = values.indexOf(Collections.max(values));
+            int distributeCount = values.get(startIndex);
+            values.set(startIndex, 0);
+            for(int i = startIndex + 1; distributeCount > 0; i++, distributeCount--) {
+                if(i == values.size()) i = 0;
+                int currentVal = values.get(i);
+                values.set(i, ++currentVal);
+            }
+            cycles++;
+            if(visitedLocations.contains(Arrays.deepToString(values.toArray()))) {
+                System.out.println(cycles - visitedLocations.indexOf(Arrays.deepToString(values.toArray())) - 1);
+                return cycles;
             } else {
-                if (startIndex == values.length) {
-                    values[index]--;
-                    values[0]++;
-                    startIndex = 0;
-                } else {
-                    values[index]--;
-                    values[startIndex]++;
-                }
+                visitedLocations.add(Arrays.deepToString(values.toArray()));
             }
         }
-    }
-
-    private int getHighestValueIndex(int[] indexValues, int value) {
-        for(int i = 0; i < indexValues.length; i++) {
-            if(indexValues[i] == value)
-                return i;
-        }
-        return -1;
-    }
-
-    public String getStringRepresentation() {
-        String stringRepresentation = "";
-
-        for(int i = 0; i < values.length; i++) {
-            stringRepresentation += values[i];
-            if(i + 1 == values.length)
-                break;
-            stringRepresentation += " ";
-        }
-        return stringRepresentation;
-    }
-
-    public int findLoop() {
-
-        boolean reachedLoopEnd = false;
-
-        while(reachedLoopEnd ==  false) {
-            step();
-            String positionRepresentation = getStringRepresentation();
-            if(visitedPositions.contains(positionRepresentation)) {
-                reachedLoopEnd = true;
-            } else {
-                visitedPositions.add(positionRepresentation);
-            }
-        }
-        nestedLoopSize = visitedPositions.size() - visitedPositions.indexOf(getStringRepresentation());
-        return visitedPositions.size();
-    }
-
-    public int getNestedLoopSize() {
-        return this.nestedLoopSize;
     }
 
     public static void main(String[] args) {
-        AdventOfCodeDaySix adventOfCodeDaySix = new AdventOfCodeDaySix(inputData);
-        System.out.println("loop size: " + adventOfCodeDaySix.findLoop());
-        System.out.println("nested loop size: " + adventOfCodeDaySix.getNestedLoopSize());
+        AdventOfCodeDaySix adventOfCodeDaySix = new AdventOfCodeDaySix();
+        System.out.println(adventOfCodeDaySix.distribute(inputData));
     }
 
-    private static final String inputData = "5\t1\t10\t0\t1\t7\t13\t14\t3\t12\t8\t10\t7\t12\t0\t6";
-
+    private static final String inputData = "5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6";
 }
